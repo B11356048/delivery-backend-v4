cea968e6a9dfbea22173e49ac7f96cc7aa04bb69
B11356048 <rimuru940927@gmail.com>
2026-06-18 03:17:29 +0900

    Update Food.js

--- models/Food.js
+++ models/Food.js
@@ -2,25 +2,62 @@ const mongoose = require("mongoose");
 
 const foodSchema = new mongoose.Schema({
 
-    storeName:String,
+    // 店家資訊
+    storeId:{
+        type:String,
+        default:""
+    },
 
+    storeName:{
+        type:String,
+        default:""
+    },
+
+    // 商品資訊
     name:{
         type:String,
         required:true
     },
 
+    category:{
+        type:String,
+        default:"其他"
+    },
+
+    desc:{
+        type:String,
+        default:""
+    },
+
+    image:{
+        type:String,
+        default:""
+    },
+
+    // 價格與庫存
     price:{
         type:Number,
         required:true
     },
-
-    desc:String,
 
     stock:{
         type:Number,
         default:50
     },
 
+    // 是否上架
+    isAvailable:{
+        type:Boolean,
+        default:true
+    },
+
+    // 銷售統計
+    salesCount:{
+        type:Number,
+        default:0
+    },
+
+    // 評價統計
     ratingSum:{
         type:Number,
         default:5
@@ -29,9 +66,7 @@ const foodSchema = new mongoose.Schema({
     ratingCount:{
         type:Number,
         default:1
-    },
-
-    image:String
+    }
 
 },{
     timestamps:true
