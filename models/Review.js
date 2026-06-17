0516c1fbf97156dc9c9df584f9072c47b51ca36b
B11356048 <rimuru940927@gmail.com>
2026-06-18 03:21:38 +0900

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -1,1 +1,40 @@
+const mongoose = require("mongoose");
 
+const reviewSchema = new mongoose.Schema({
+
+    foodId:{
+        type:String,
+        required:true
+    },
+
+    userId:{
+        type:String,
+        required:true
+    },
+
+    userName:{
+        type:String,
+        default:""
+    },
+
+    stars:{
+        type:Number,
+        min:1,
+        max:5,
+        required:true
+    },
+
+    comment:{
+        type:String,
+        default:""
+    }
+
+},{
+    timestamps:true
+});
+
+module.exports =
+mongoose.model(
+    "Review",
+    reviewSchema
+);
