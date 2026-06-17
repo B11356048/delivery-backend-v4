d0d4072dd098637532dcc2c70d8ba2dbfdebd93c
B11356048 <rimuru940927@gmail.com>
2026-06-18 05:21:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1155,3 +1155,17 @@ async(req,res)=>{
     }
 
 });
+/* =====================
+   啟動伺服器
+===================== */
+
+const PORT =
+process.env.PORT || 10000;
+
+app.listen(PORT,()=>{
+
+    console.log(
+        `Server Running ${PORT}`
+    );
+
+});
