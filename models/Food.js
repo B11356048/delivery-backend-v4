3aa62628b2be7a3eef0d34b1eca286377259f0cc
B11356048 <rimuru940927@gmail.com>
2026-06-18 06:40:27 +0900

    Update Food.js

--- models/Food.js
+++ models/Food.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const FoodSchema = new mongoose.Schema({
-    storeName: { type: String, required: true }, // 店家名稱
-    foodName: { type: String, required: true },  // 餐點名稱
-    price: { type: Number, required: true },     // 價格
-    desc: { type: String, default: '' },         // 餐點描述
+    storeName: { type: String, required: true },  // 店家名稱
+    foodName: { type: String, required: true },   // 餐點名稱
+    price: { type: Number, required: true },      // 價格
+    desc: { type: String, default: '' },          // 餐點描述
     createdAt: { type: Date, default: Date.now }
 });
 
