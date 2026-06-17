8301602154b127332fb9621f39a423da10fbcf9f
B11356048 <rimuru940927@gmail.com>
2026-06-18 06:27:55 +0900

    Update Food.js

--- models/Food.js
+++ models/Food.js
@@ -1,79 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const foodSchema = new mongoose.Schema({
-
-    // 店家資訊
-    storeId:{
-        type:String,
-        default:""
-    },
-
-    storeName:{
-        type:String,
-        default:""
-    },
-
-    // 商品資訊
-    name:{
-        type:String,
-        required:true
-    },
-
-    category:{
-        type:String,
-        default:"其他"
-    },
-
-    desc:{
-        type:String,
-        default:""
-    },
-
-    image:{
-        type:String,
-        default:""
-    },
-
-    // 價格與庫存
-    price:{
-        type:Number,
-        required:true
-    },
-
-    stock:{
-        type:Number,
-        default:50
-    },
-
-    // 是否上架
-    isAvailable:{
-        type:Boolean,
-        default:true
-    },
-
-    // 銷售統計
-    salesCount:{
-        type:Number,
-        default:0
-    },
-
-    // 評價統計
-    ratingSum:{
-        type:Number,
-        default:5
-    },
-
-    ratingCount:{
-        type:Number,
-        default:1
-    }
-
-},{
-    timestamps:true
+const FoodSchema = new mongoose.Schema({
+    storeName: { type: String, required: true }, // 店家名稱
+    foodName: { type: String, required: true },  // 餐點名稱
+    price: { type: Number, required: true },     // 價格
+    desc: { type: String, default: '' },         // 餐點描述
+    createdAt: { type: Date, default: Date.now }
 });
 
-module.exports =
-mongoose.model(
-    "Food",
-    foodSchema
-);
+module.exports = mongoose.model('Food', FoodSchema);
